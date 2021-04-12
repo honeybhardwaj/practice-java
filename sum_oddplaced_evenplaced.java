a445946a3d0eb9b58d3ab7f5ad9d17cca340b796
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-04-12 22:36:57 +0530

    code added

---
+++ sum_oddplaced_evenplaced.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class sum_oddplaced_evenplaced {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long num = in.nextLong();
+        int pos=1;
+        long oddsum=0,evensum=0;
+        while(num>0){
+            int digit=(int) num%10;
+            if(pos % 2 == 0){
+                evensum+=digit;
+            }
+            else{
+                oddsum+=digit;
+            }
+            pos++;
+            num=num/10;
+        }
+        System.out.println(oddsum);
+        System.out.println(evensum);
+    }
+}
