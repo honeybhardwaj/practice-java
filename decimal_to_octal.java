600578a32d78d742bcb87534e8fcb6e4c3f29a75
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-11 21:59:06 +0530

    decimal to octal conversion

---
+++ decimal_to_octal.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class decimal_to_octal {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        int num=in.nextInt();
+        int ans=0, count=0, digit;
+        while(num>0){
+            digit = num%8;
+            ans= (int) (ans+digit*Math.pow(10,count));
+            num=num/8;
+            count++;
+        }
+        System.out.println(ans);
+
+    }
+}
