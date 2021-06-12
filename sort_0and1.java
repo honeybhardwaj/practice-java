1a08d517cf7dabfdf5c9fa9beb720917efce99c8
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-13 03:23:54 +0530

    sorting of array of 0 and 1

---
+++ sort_0and1.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class sort_0and1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i]=in.nextInt();
+        }
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            if(arr[low]==1){
+                int temp = arr[low];
+                arr[low]=arr[high];
+                arr[high]=temp;
+
+                high--;
+            }
+            else{
+                low++;
+            }
+
+        }
+        for(int val: arr){
+            System.out.print(val+" ");
+        }
+    }
+}
