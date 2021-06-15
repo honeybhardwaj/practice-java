23ba9c8847c1867f803bb76c82710843f4dc1dd6
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-15 22:07:21 +0530

    bubble sort in array

---
+++ bubble_sort.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class bubble_sort {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr []= new int[n];
+        int i,j;
+        for(i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        for(i=0;i<n-1;i++){
+            for(j=0;j<n-i-1;j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int val: arr){
+            System.out.println(val);
+        }
+    }
+}
