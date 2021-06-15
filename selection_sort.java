d33795db90fb79bd6e216f1d110d1cff9f1ebea8
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-15 22:07:43 +0530

    selection sort in array

---
+++ selection_sort.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class selection_sort {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int [] arr = new int[n];
+        int i,j;
+        for(i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+
+        for(i=0;i<n-1;i++){
+            int imin=i;
+            for(j=i+1;j<n;j++){
+                if(arr[j]<arr[imin]){
+                    imin=j;
+                }
+            }
+            int temp=arr[imin];
+            arr[imin]=arr[i];
+            arr[i]=temp;
+        }
+        for(int val: arr){
+            System.out.println(val);
+        }
+    }
+}
