e6e3645b41bcfdf861f1e43e696d316aab025b3a
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-16 22:57:28 +0530

    binary search in array

---
+++ Binary_Search.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Binary_Search {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        Arrays.sort(arr);
+        int m=in.nextInt();
+        System.out.println(BinarySearch(arr,m,n));
+    }
+    public static int BinarySearch(int[] arr,int m,int n){
+        int lo=0;
+        int hi=n-1;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(arr[mid]==m){
+                return mid;
+            }
+            else if(arr[mid]<m){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        return -1;
+    }
+}
