8b6f5e936d919bf471e0268754a0672376aed0dc
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-17 04:49:55 +0530

    wave print of 2d array row wise

---
+++ wavePrint_rowise.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class wavePrint_rowise {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int [][] arr=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=in.nextInt();
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(i%2==1){
+                for(int j=0;j<n;j++){
+                    System.out.print(arr[i][j]);
+                }
+            }
+            else{
+                for(int j = n-1;j>=0;j--){
+                    System.out.print(arr[i][j]);
+                }
+            }
+        }
+
+    }
+}
