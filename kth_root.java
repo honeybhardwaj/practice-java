912c17ff6a93b9cd238156aec69bece9c1ea7a3c
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-18 05:58:24 +0530

    kth root of a number

---
+++ kth_root.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class kth_root {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t>0){
+            long n= in.nextLong();
+            int k= in.nextInt();
+            long lo=0;
+            long hi=n;
+            while(lo<=hi) {
+                long mid = (lo+hi)/2;
+                if((long) (Math.pow(mid,k))==n) {
+//					System.out.println(mid);
+					hi=mid;
+                    break;
+                }
+                else if((long) (Math.pow(mid,k))<n) {
+                    lo = mid+1;
+                }
+                else {
+                    hi = mid-1;
+                }
+            }
+            System.out.println(hi);
+            t--;
+        }
+    }
+}
+
