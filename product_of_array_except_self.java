9d8519db2b952b8e21c3053871911fb70d3b34fd
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-23 23:19:36 +0530

    product of array except self without nested loops and division operator

---
+++ product_of_array_except_self.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class product_of_array_except_self {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int []arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        int []left = new int[n];
+        int []right= new int[n];
+        left[0]=1;
+        for (int i=1;i<n;i++){
+            left[i]=left[i-1]*arr[i-1];
+        }
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            right[i]=right[i+1]*arr[i+1];
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=right[i]*left[i];
+        }
+        for(int val: arr){
+            System.out.print(val+" ");
+        }
+    }
+}
