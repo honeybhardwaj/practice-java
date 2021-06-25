b6dd373a190edf05ddbb7200133285fbb4acbba1
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-25 23:05:30 +0530

    alex goes to shopping

---
+++ alex_goes_shopping.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class alex_goes_shopping {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int []arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        int q = in.nextInt();
+        while(q>0){
+            int a = in.nextInt();
+            int k = in.nextInt();
+            int count = 0;
+            for(int i=0;i<n;i++){
+                if(arr[i]%a==0){
+                    count++;
+                }
+                System.out.println(count);
+            }
+            if(count>=k){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+            q--;
+        }
+
+    }
+}
