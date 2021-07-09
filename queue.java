93a7f21a7b8c73cfd34b0c6e8ec546f1525156ca
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-07-10 00:55:41 +0530

    queue using arrays

---
+++ queue.java
@@ -0,0 +1,63 @@
+package com.Coding_Blocks;
+
+public class queue {
+    private int[] arr;
+    private int front;
+    private int size;
+
+    public queue(){
+        arr= new int[5];
+        front=0;
+        size=0;
+    }
+    public queue(int capa){
+        arr=new int[capa];
+        front=0;
+        size=0;
+    }
+    public int getfront(){
+        return  arr[front];
+    }
+    public boolean isfull(){
+        return size == arr.length;
+    }
+    public boolean isempty(){
+        return size == 0;
+    }
+    public void enqueue(int item) throws Exception{
+        if(isfull()){
+            throw new Exception("Queue is full");
+        }
+        arr[(front+size)%arr.length]=item;
+        size++;
+    }
+    public int dequeue() throws Exception{
+        if(isempty()){
+            throw new Exception("Queue is empty");
+        }
+        int temp=arr[front];
+        size--;
+        front=(front+1)%arr.length;
+        return temp;
+    }
+    public void disp(){
+        for(int i=0;i<size;i++){
+            System.out.print(arr[(i+front)%arr.length]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws Exception{
+        queue qq= new queue();
+        qq.enqueue(10);
+        qq.enqueue(20);
+        qq.enqueue(30);
+        qq.enqueue(40);
+        qq.enqueue(50);
+        qq.disp();
+        qq.dequeue();
+        qq.disp();
+        qq.enqueue(10);
+        qq.disp();
+    }
+}
